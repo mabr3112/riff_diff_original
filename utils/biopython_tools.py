03a40521535cfe4a0f63e3758fe77fdec8d3e063
Markus Braun <markus.braun95@gmail.com>
2023-03-11 17:51:29 +0100

    Fixed bug with renaming poses in Biopython_tools.py module

--- utils/biopython_tools.py
+++ utils/biopython_tools.py
@@ -1,6 +1,7 @@
 import Bio
 from Bio.PDB import PDBIO
 import Bio.PDB
+import copy
 
 def load_structure_from_pdbfile(path_to_pdb: str, all_models=False) -> Bio.PDB.Structure:
     '''AAA'''
@@ -21,22 +22,32 @@ def residue_mapping_from_motif(motif_old: list, motif_new: list) -> dict:
 
 def renumber_pose_by_residue_mapping(pose: Bio.PDB.Structure.Structure, residue_mapping: dict) -> Bio.PDB.Structure.Structure:
     '''AAA'''
+    # deepcopy pose and detach all residues from chains.
+    out_pose = copy.deepcopy(pose)
+    ch = [chain.id for chain in out_pose.get_chains()]
+    for chain in ch:
+        residues = [res.id for res in out_pose[chain].get_residues()]
+        [out_pose[chain].detach_child(resi) for resi in residues]
+
+    # collect residues with renumbered ids and chains into one list:
     for old_res, new_res in residue_mapping.items():
-        # change residue ID
-        pose[old_res[0]][(" ", old_res[1], " ")].id = (" ", new_res[1], " ")
+        # remove old residue from original pose
+        res = pose[old_res[0]][(" ", old_res[1], " ")]
+        pose[old_res[0]].detach_child((" ", old_res[1], " "))
+        res.detach_parent()
 
-        # change Chain if Chain-names in residue mapping don't match:
-        if new_res[0] != old_res[0]: 
-            residue = pose[old_res[0]][(" ", new_res[1], " ")]
-            pose[old_res[0]].detach_child((" ", new_res[1], " "))
-            pose[new_res[0]].add(residue)
-            
+        # set new residue ID
+        res.id = (" ", new_res[1], " ")
+
+        # add to appropriate chain (residue mapping) in out_pose
+        out_pose[new_res[0]].add(res)
+
     # remove chains from pose that are empty:
-    chain_ids = [x.id for x in pose] # for some reason, iterating over chains in struct directly does not work here...
+    chain_ids = [x.id for x in out_pose] # for some reason, iterating over chains in struct directly does not work here...
     for chain_id in chain_ids:
-        if not pose[chain_id].__dict__["child_dict"]: pose.detach_child(chain_id)
+        if not out_pose[chain_id].__dict__["child_dict"]: out_pose.detach_child(chain_id)
 
-    return pose
+    return out_pose
 
 def renumber_pdb_by_residue_mapping(pose_path: str, residue_mapping: dict, out_pdb_path=None) -> str:
     '''AAA'''
